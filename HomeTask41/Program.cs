c09870e0bbefbdab4a4c1007fb25c0c1de5491a3
Bitnik <kukiyshka@gmail.com>
2023-02-12 22:14:37 +0300

    home task complite

---
+++ HomeTask41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №41. Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Write("Введите числа через запятую или пробел: ");
+string numbers = Console.ReadLine()!;
+numbers = numbers.Replace(",", " ");    //заменяем запятые на пробелы
+//далее создаем массив из подстрок, разделенных пробелами и удаляем пустые (только с пробелами) подстроки
+string[] Numbers = numbers.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+int NumbersArray(int[] intNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < Numbers.Length; i++)
+    {
+        intNumbers[i] = int.Parse(Numbers[i]);
+        if (intNumbers[i] > 0) count++;
+    }
+    Console.WriteLine($"Пользователь ввел чисел больше нуля в количестве {count}");
+    return count;
+}
+int[] intNumbers = new int[Numbers.Length];
+NumbersArray(intNumbers);
---
+++ HomeTaskWithStar/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача со звездочкой. Напишите программу, которая реализует обход введенного двумерного массива,
+// начиная с крайнего нижнего левого элемента против часовой стрелки.
+
+Console.Write("Введите количество строк массива: ");
+int i = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int j = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[i, j];                                // объявляем массив
+
+void PrintArray(int[,] matrix)                                // создаем массив
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)                                 // инициализируем массив
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 20);
+            Console.Write(matrix[i, j].ToString().PadLeft(4));
+        }
+        Console.WriteLine();
+    }
+}
+
+int count = 0;
+void PrintArray1(int[,] matrix)                                      // передаем массив в метод
+{
+    if (count == matrix.Length - 1)                                  // условие выхода из рекурсии
+    {
+        return;
+    }
+    for (j = count; j < matrix.GetLength(1) - count; j++)            // делаем проход вправо
+    {
+        i = matrix.GetLength(0) - 1 - count;
+        Console.Write($"{matrix[i, j]} ");
+    }
+    for (i = i - 1; i >= count; i--)                                 // делаем проход вверх
+    {
+        j = matrix.GetLength(1) - 1 - count;
+        Console.Write($"{matrix[i, j]} ");
+    }
+    for (j = j - 1; j >= count; j--)                                 // делаем проход влево
+    {
+        i = count;
+        Console.Write($"{matrix[i, j]} ");
+    }
+    for (i = i + 1; i < matrix.GetLength(0) - 1 - count; i++)        // делаем проход вниз
+    {
+        j = count;
+        Console.Write($"{matrix[i, j]} ");
+    }
+    count++;                                // с помощью счетчика отсекаем пройденные стороны
+    PrintArray1(matrix);
+}
+
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+Console.WriteLine();
+PrintArray1(matrix);
